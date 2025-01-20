57c52567cfc7e603264c60a9ea15b7125dab975f
Missing <dnt0@gmail.com>
2025-01-19 18:12:26 -0800

    Initial GUI

---
+++ main.py
@@ -0,0 +1,65 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTimer
+import pyqtgraph as pg
+import serial
+import time
+
+uiclass, baseclass = pg.Qt.loadUiType("main.ui")
+
+class MainWindow(uiclass, baseclass):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.ser = serial.Serial()
+        self.ser.port = "/dev/ttyACM0"
+        self.ser.baudrate = 115200
+        self.ser.timeout = 5
+
+        self.dataLength = 6
+
+        self.plot(
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], # Hours
+            [30, 32, 34, 32, 33, 31, 29, 32, 35, 45], # Temperature
+        )
+
+        self.pushButton.clicked.connect(self.start)
+        self.pushButton_2.clicked.connect(self.stop)
+
+        self.timer = QTimer(self)
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_data)
+
+    def plot(self, hour, temperature):
+        self.graphWidget.plot(hour, temperature)
+
+    def start(self):
+        if (self.ser.is_open != True):
+            self.ser.open()
+            self.ser.reset_input_buffer()
+
+        time.sleep(2)
+        self.ser.write(b'start\n')
+        self.timer.start()
+
+    def stop(self):
+        self.timer.stop()
+        time.sleep(0.5)
+        self.ser.reset_input_buffer()
+        self.ser.close()
+
+    def update_data(self):
+        if self.ser.in_waiting:
+            incomingData = self.ser.readline()
+            incomingData = incomingData.decode('utf-8')
+            dataRow = incomingData.removesuffix('\r\n').split(',')
+
+            if len(dataRow) == self.dataLength:
+                print(dataRow)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
